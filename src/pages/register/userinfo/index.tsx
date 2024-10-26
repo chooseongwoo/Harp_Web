39ff7875ceae81b20d7a4726cbf163ab6fd17bee
추성우 <cntjddn715@gmail.com>
2024-10-26 17:41:16 +0900

    chore :: 생년월일 유효성 검사

--- src/pages/register/userinfo/index.tsx
+++ src/pages/register/userinfo/index.tsx
@@ -38,9 +38,28 @@ const UserInfo = () => {
     });
   };
 
+  const isValidDate = (dateString: string): boolean => {
+    if (dateString.length !== 10) return false;
+    const [year, month, day] = dateString.split('/').map(Number);
+    const inputDate = new Date(year, month - 1, day);
+    const currentDate = new Date();
+
+    return (
+      inputDate.getFullYear() === year &&
+      inputDate.getMonth() + 1 === month &&
+      inputDate.getDate() === day &&
+      inputDate <= currentDate
+    );
+  };
+
   const isFormValid = () => {
     const { username, birthday, gender } = userInfos;
-    return username.length >= 2 && birthday && gender;
+    return (
+      username.length >= 2 &&
+      birthday.length === 10 &&
+      isValidDate(birthday) &&
+      gender
+    );
   };
 
   return (

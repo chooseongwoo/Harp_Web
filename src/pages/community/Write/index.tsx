bf0ea88e4b7a3db41febadecd277e84f2763f3ce
rkdalswl718 <rkdalswl070718@gmail.com>
2024-10-31 12:27:44 +0900

    style :: input

--- src/pages/community/Write/index.tsx
+++ src/pages/community/Write/index.tsx
@@ -3,17 +3,30 @@ import React from 'react';
 
 //파일
 import CategoryModal from 'components/community/CategoryModal';
-import * as _ from './style'
+import * as _ from './style';
 import Header from 'components/Header';
 import DownArrow from 'assets/Icon/DownArrow';
+import { theme } from 'lib/utils/style/theme';
 
 const Write = () => {
   return (
     <_.Write_Layout>
-        <Header title='글 쓰기' buttonState='게시'/>
+      <Header title="글 쓰기" buttonState="게시" />
       <_.Write_Container>
-      <CategoryModal onClose={()=>{}} isOpen={false} onSelectCategory={()=>{}} />
-        <_.Write_ModalButton>게시글 카테고리 선택하기 <DownArrow/></_.Write_ModalButton>
+        <CategoryModal
+          onClose={() => {}}
+          isOpen={false}
+          onSelectCategory={() => {}}
+        />
+        <_.Write_ModalButton onClick={() => {}}>
+          게시글 카테고리 선택하기 <DownArrow color={theme.gray['3.5']} />
+        </_.Write_ModalButton>
+        <_.Write_TitleInput placeholder="제목을 입력하세요..." />
+        <_.Write_Line/>
+        <_.Write_DesInput
+          placeholder="- 커뮤니티 가이드라인을 준수하며, 서로를 배려하고 존중하는 글을 작성해주세요.
+- 불필요한 비방이나 공격적인 표현은 자제하고, 모두가 즐겁게 소통할 수 있는 환경을 만들어주세요."
+        ></_.Write_DesInput>
       </_.Write_Container>
     </_.Write_Layout>
   );
--- src/pages/community/Write/style.ts
+++ src/pages/community/Write/style.ts
@@ -17,11 +17,42 @@ export const Write_Container = styled.div`
 export const Write_ModalButton = styled.button`
   display: flex;
   width: 100%;
-padding: 14px 16px;
-justify-content: space-between;
-align-items: center;
-background-color: #fff;
-border: 1px solid ${theme.gray[1]};
-border-radius: 8px;
-font-size: 15px;
+  padding: 14px 16px;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  border: 1px solid ${theme.gray[1]};
+  border-radius: 8px;
+  font-size: 15px;
+  margin: 10px 0 5px 0;
+`;
+
+export const Write_TitleInput = styled.input`
+  border: none;
+  outline: none;
+  color: ${theme.gray.black};
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  margin-top: 10px;
+  ::placeholder {
+    color: red;
+  }
+`;
+
+export const Write_Line = styled.div`
+  border: 1px solid ${theme.sub[1]};
+  margin: 10px 0;
 `
+
+export const Write_DesInput = styled.textarea`
+  border: none;
+  outline: none;
+  color: ${theme.gray.black};
+  font-size: 15px;
+  resize: none;
+  min-height: 400px;
+  ::placeholder {
+    color: red;
+  }
+`;

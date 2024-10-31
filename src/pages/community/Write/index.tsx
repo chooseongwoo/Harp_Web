08f7e2eab5b8d3c5bc032f8c0db970afcf74ad0a
rkdalswl718 <rkdalswl070718@gmail.com>
2024-10-31 14:59:55 +0900

    chore :: location icon export

--- src/pages/community/Write/index.tsx
+++ src/pages/community/Write/index.tsx
@@ -7,6 +7,8 @@ import * as _ from './style';
 import Header from 'components/Header';
 import DownArrow from 'assets/Icon/DownArrow';
 import { theme } from 'lib/utils/style/theme';
+import Image from 'assets/image/Image';
+import Location_g from 'assets/image/Location-g';
 
 const Write = () => {
   return (
@@ -22,11 +24,19 @@ const Write = () => {
           게시글 카테고리 선택하기 <DownArrow color={theme.gray['3.5']} />
         </_.Write_ModalButton>
         <_.Write_TitleInput placeholder="제목을 입력하세요..." />
-        <_.Write_Line/>
+        <_.Write_Line />
         <_.Write_DesInput
           placeholder="- 커뮤니티 가이드라인을 준수하며, 서로를 배려하고 존중하는 글을 작성해주세요.
 - 불필요한 비방이나 공격적인 표현은 자제하고, 모두가 즐겁게 소통할 수 있는 환경을 만들어주세요."
         ></_.Write_DesInput>
+        <_.Write_BottomContainer>
+          <_.Write_PhotoButton>
+            <Image /> 사진
+          </_.Write_PhotoButton>
+          <_.Write_LocationButton>
+            <Location_g />
+          </_.Write_LocationButton>
+        </_.Write_BottomContainer>
       </_.Write_Container>
     </_.Write_Layout>
   );
--- src/pages/community/Write/style.ts
+++ src/pages/community/Write/style.ts
@@ -2,7 +2,6 @@ import { theme } from 'lib/utils/style/theme';
 import styled from 'styled-components';
 
 export const Write_Layout = styled.div`
-  position: fixed;
   width: 100%;
   height: calc(100vh - 40px);
 `;
@@ -22,6 +21,7 @@ export const Write_ModalButton = styled.button`
   align-items: center;
   background-color: #fff;
   border: 1px solid ${theme.gray[1]};
+  color: ${theme.gray.black};
   border-radius: 8px;
   font-size: 15px;
   margin: 10px 0 5px 0;
@@ -31,12 +31,12 @@ export const Write_TitleInput = styled.input`
   border: none;
   outline: none;
   color: ${theme.gray.black};
-  font-size: 18px;
+  font-size: 20px;
   font-style: normal;
   font-weight: 600;
   margin-top: 10px;
-  ::placeholder {
-    color: red;
+  &::placeholder {
+    color: ${theme.gray[2.5]};
   }
 `;
 
@@ -49,10 +49,55 @@ export const Write_DesInput = styled.textarea`
   border: none;
   outline: none;
   color: ${theme.gray.black};
-  font-size: 15px;
+  font-size: 16px;
   resize: none;
   min-height: 400px;
-  ::placeholder {
-    color: red;
+  &::placeholder {
+    color: ${theme.gray[2.5]};
   }
+`;
+
+export const Write_BottomContainer = styled.div`
+  position: fixed;
+  bottom: 100px;
+  left: 0;
+  display: flex;
+  width: 100%;
+  padding: 13px 20px;
+  background-color: white;
+  gap: 12px;
+  border-top: 1px solid ${theme.gray[1]};
+  `;
+
+export const Write_PhotoButton = styled.button`
+gap: 3px;
+  display: flex;
+  align-items: center;
+  border: none;
+  font-size: 16px;
+color: ${theme.gray[3]};
+  cursor: pointer;
+  background-color: #fff;
+`;
+
+export const Write_LocationButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${theme.gray[1]};
+  border: 1px solid ${theme.gray[2]};
+  border-radius: 8px;
+  padding: 10px;
+  font-size: 15px;
+  margin: 10px 0;
+  cursor: pointer;
+`;
+
+export const KeyboardToggleButton = styled.button`
+  position: fixed;
+  bottom: 60px;
+  right: 20px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
 `;

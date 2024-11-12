c31175a954507866b48e5029ee2386b39ab71141
추성우 <cntjddn715@gmail.com>
2024-11-12 16:38:45 +0900

    add(#137) :: 답글 열기 & 닫기

--- src/components/community/Comment/index.tsx
+++ src/components/community/Comment/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // 파일
 import * as _ from './style';
@@ -9,6 +9,12 @@ import { comment } from 'types/community';
 import { getDayMinuteCounter } from 'lib/utils/getDayMinuteCounter';
 
 const Comments = ({ comment }: { comment: comment }) => {
+  const [isOpened, setIsOpened] = useState(false);
+  const handleReplyOpen = () => {
+    if (comment.replies.length > 0) {
+      setIsOpened(!isOpened);
+    }
+  };
   return (
     <_.Comment_Layout>
       <_.Comment_Profile src={comment.author.profileImg} />
@@ -18,14 +24,16 @@ const Comments = ({ comment }: { comment: comment }) => {
           {getDayMinuteCounter(comment.updatedAt)}
         </_.Comment_Time>
         <_.Comment_Description>{comment.des}</_.Comment_Description>
-        <_.Comment_Reply>
+        <_.Comment_Reply onClick={handleReplyOpen}>
           <Comment width="14" height="14" /> 답글 {comment.replies.length}
         </_.Comment_Reply>
-        <_.Comment_ReplyComments>
-          {comment.replies.map((reply) => (
-            <ReplyComment key={reply.commnetsId} replyComment={reply} />
-          ))}
-        </_.Comment_ReplyComments>
+        {isOpened ? (
+          <_.Comment_ReplyComments>
+            {comment.replies.map((reply) => (
+              <ReplyComment key={reply.commnetsId} replyComment={reply} />
+            ))}
+          </_.Comment_ReplyComments>
+        ) : null}
       </_.Comment_ColContainer>
       <KebabMenu onClick={() => {}} />
     </_.Comment_Layout>

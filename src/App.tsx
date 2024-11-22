dd2b6ea14bcc49e67eeb2413e5fd1066f3de2298
추성우 <cntjddn715@gmail.com>
2024-11-22 09:46:14 +0900

    add(#147) :: 로그인 스택

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import React, { useEffect } from 'react';
-import Routes from './routes';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Stack } from 'stackflow';
 
 const App = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.pathname === '/splash') {
-      const timer = setTimeout(() => {
-        navigate('/auth');
-      }, 1500);
-
-      return () => clearTimeout(timer);
-    }
-  }, [location.pathname, navigate]);
   return (
     <>
-      <Routes />
+      <Stack />
     </>
   );
 };
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,5 +1,6 @@
 // 라이브러리
 import React from 'react';
+import { AppScreen } from '@stackflow/plugin-basic-ui';
 
 // 파일
 import * as _ from './style';
@@ -16,38 +17,39 @@ const Auth = () => {
   };
 
   return (
-    <_.Auth_Container>
-      <_.Auth_Title_Layout>
-        <_.Auth_Bubble_1 />
-        <_.Auth_Bubble_2 />
-        <_.Auth_Bubble_3 />
-        <_.Auth_Title_Big>{title}</_.Auth_Title_Big>
-        <_.Auth_Title_Small>하프와 함께하는 여행 계획</_.Auth_Title_Small>
-      </_.Auth_Title_Layout>
-      <_.Auth_Button_Layout>
-        <_.Auth_Button_Ballon>
-          <_.Auth_Button_Ballon_Title>
-            3초만에 하는 빠른 회원가입 🚀
-          </_.Auth_Button_Ballon_Title>
-          <_.Auth_Button_Ballon_SVG>
-            <RoundedTriangle />
-          </_.Auth_Button_Ballon_SVG>
-        </_.Auth_Button_Ballon>
-        <AuthButton
-          onClick={handleKakaoLogin}
-          background="#FEE500"
-          icon={<KakaoTalk />}
-          content="카카오 로그인"
-        />
-        <AuthButton
-          onClick={handleKakaoLogin}
-          background="#FFF"
-          icon={<Google />}
-          content="구글 로그인"
-        />
-        <_.Auth_Button_Hint>로그인에 어려움이 있으신가요?</_.Auth_Button_Hint>
-      </_.Auth_Button_Layout>
-    </_.Auth_Container>
+    <AppScreen>
+      <_.Auth_Container>
+        <_.Auth_Title_Layout>
+          <_.Auth_Bubble_1 />
+          <_.Auth_Bubble_2 />
+          <_.Auth_Bubble_3 />
+          <_.Auth_Title_Big>{title}</_.Auth_Title_Big>
+          <_.Auth_Title_Small>하프와 함께하는 여행 계획</_.Auth_Title_Small>
+        </_.Auth_Title_Layout>
+        <_.Auth_Button_Layout>
+          <_.Auth_Button_Ballon>
+            <_.Auth_Button_Ballon_Title>
+              3초만에 하는 빠른 회원가입 🚀
+            </_.Auth_Button_Ballon_Title>
+            <_.Auth_Button_Ballon_SVG>
+              <RoundedTriangle />
+            </_.Auth_Button_Ballon_SVG>
+          </_.Auth_Button_Ballon>
+          <AuthButton
+            onClick={handleKakaoLogin}
+            background="#FEE500"
+            icon={<KakaoTalk />}
+            content="카카오 로그인"
+          />
+          <AuthButton
+            onClick={handleKakaoLogin}
+            background="#FFF"
+            icon={<Google />}
+            content="구글 로그인"
+          />
+        </_.Auth_Button_Layout>
+      </_.Auth_Container>
+    </AppScreen>
   );
 };
 
